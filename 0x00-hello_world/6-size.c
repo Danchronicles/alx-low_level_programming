5eab3e2d363a7a843a13b7778a17f6741a5ac61b
Danchronicles <dankipchumba669@gmail.com>
2023-09-07 13:18:08 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,3 @@ int main(void)
 	printf("Size of a long int: 4 byte(s)\n");
 	printf("Size of a long long int: 8 byte(s)\n");
 	printf("Size of a float: 4 byte(s)\n");
-	printf("julien@ubuntu:~/c/0x00$ ./size64\n");
-	printf("Size of a char: 1 byte(s)\n");
-	printf("Size of an int: 4 byte(s)\n");
-	printf("Size of a long int: 8 byte(s)\n");
-	printf("Size of a long long int: 8 byte(s)\n");
-	printf("Size of a float: 4 byte(s)\n");
-	return (0);
-}
