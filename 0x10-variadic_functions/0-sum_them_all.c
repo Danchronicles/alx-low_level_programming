6f7e3cb7e3a5a1f2b4b52c12fa3d42884aa334cc
Danchronicles <dankipchumba669@gmail.com>
2023-10-12 11:22:05 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,18 +8,19 @@
 int sum_them_all(const unsigned int n, ...);
 int main (void){
 int sum;
-sum = sum_them_all(5,6);
+sum = sum_them_all(5,1,2,3,4,5);
 printf("%d\n", sum);
 return (0);
 }
 int sum_them_all(const unsigned int n, ...){
 	int i;
-	int result;
+	int result = 0;
 	va_list sumthemall;
 	va_start (sumthemall, n);
 	for(i = 0; i < 5; i++){
 		result = result + va_arg(sumthemall, int);
 	}
+	va_end(sumthemall);
 	return (result);
 }
 
