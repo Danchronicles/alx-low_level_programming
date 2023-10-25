e6211d065f9ca05f1461a78b8b4e00461a7b12dc
Danchronicles <dankipchumba669@gmail.com>
2023-10-25 04:28:57 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,21 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main (void){
+char str[] = "Hello, World!";
+ _puts_recursion(str);
+ return(0);
+}
+void _puts_recursion(char *s){
+if(*s == '\0')
+{
+	putchar('\n');
+}
+return;
+putchar(*s);
+_puts_recursion(s + 1);
+}
