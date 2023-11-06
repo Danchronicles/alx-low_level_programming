298da1b325741f5bfeb2f8d8f1d63f717d7aa2ec
Danchronicles <dankipchumba669@gmail.com>
2023-11-06 03:14:18 -0800

    task 0 structures in C

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+#include<stdio.h>
+/**
+ *
+ *main - check the code
+ *
+ * Return: Always 0.
+ */
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
