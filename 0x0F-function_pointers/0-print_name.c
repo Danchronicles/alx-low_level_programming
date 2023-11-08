b5c5a7b6dc37813328589a9f0af22b4ef305126e
Danchronicles <dankipchumba669@gmail.com>
2023-11-08 13:23:39 -0800

    task 0mof function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - a funtion that prints a name
+ *
+ * @f: variable to hold the string name
+ * @name: name of the person
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
