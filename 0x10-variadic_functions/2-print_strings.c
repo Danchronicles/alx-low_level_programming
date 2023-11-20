5d5f9328ad46c4c50986a4063c2183667ebbd168
Danchronicles <dankipchumba669@gmail.com>
2023-11-20 01:47:00 -0800

    tas2 of variadic functions betty

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,15 +13,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	const char *str;
-	
+
 	va_list args;
+
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, const char *);
 		printf("%s", str);
-		if (separator != NULL && i < n -1)
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
 	va_end(args);
