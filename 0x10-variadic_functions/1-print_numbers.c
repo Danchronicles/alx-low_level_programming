14e0c5e809ad4124016e7dd5f67bd5a205fb4a8c
Danchronicles <dankipchumba669@gmail.com>
2023-11-20 00:36:48 -0800

    printnumbers-task-variadic-functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -24,13 +24,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(args, int));
 		if (separator != NULL && i < n - 1)
-        {
-                printf("%s", separator);
-        }
-	}
-	if (separator != NULL && i < n - 1)
-	{
-		printf("%s", separator);
+		{
+			printf("%s", separator);
+		}
 	}
 	va_end(args);
 	printf("\n");
