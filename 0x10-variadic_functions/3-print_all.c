8f755d9a6a622adbe5f998e52d8a7f29136c954e
Danchronicles <dankipchumba669@gmail.com>
2023-11-21 04:39:54 -0800

    checker 1 task3 variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -41,12 +41,13 @@ void print_all(const char * const format, ...)
 			s = va_arg(args, char*);
 			if(s == NULL)
 			{
-				printf("nil");
+				printf("(nil)");
 			}
 			else
 			{
 				printf("%s", s);
 			}
+			break;
 			default:
 			break;
 		}
